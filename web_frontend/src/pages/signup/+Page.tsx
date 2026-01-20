3b606fae8f3b06cce91a9adface2eccb9d89b83f
Luc Brinkman <75385930+lucbrinkman@users.noreply.github.com>
2026-01-20 10:09:15 +0100

    feat: add remaining SPA pages

---
+++ web_frontend/src/pages/signup/+Page.tsx
@@ -0,0 +1,10 @@
+import Layout from "@/components/Layout";
+import Signup from "@/views/Signup";
+
+export default function SignupPage() {
+  return (
+    <Layout>
+      <Signup />
+    </Layout>
+  );
+}
