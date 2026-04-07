085c15a1e4b79945ab939936210386dfd4a6de17
Luc Brinkman <75385930+lucbrinkman@users.noreply.github.com>
2026-04-07 12:02:17 +0000

    style: format alembic merge migration

--- alembic/versions/4fedfe9102c9_merge_discord_schema_and_zoom_migrations.py
+++ alembic/versions/4fedfe9102c9_merge_discord_schema_and_zoom_migrations.py
@@ -5,12 +5,13 @@ Revises: 180fdfc169c5, c0dd40a430ae
 Create Date: 2026-04-07 11:40:46.412486
 
 """
+
 from typing import Sequence, Union
 
 
 # revision identifiers, used by Alembic.
-revision: str = '4fedfe9102c9'
-down_revision: Union[str, None] = ('180fdfc169c5', 'c0dd40a430ae')
+revision: str = "4fedfe9102c9"
+down_revision: Union[str, None] = ("180fdfc169c5", "c0dd40a430ae")
 branch_labels: Union[str, Sequence[str], None] = None
 depends_on: Union[str, Sequence[str], None] = None
 
